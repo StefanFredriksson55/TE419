a79f485a81458cdf95777d7c0c3248eac43816a9
stefanfredriksson55 <stefan.fredriksson@gmail.com>
2019-09-17 13:46:51 +0200

    2019-09-17

---
+++ ListorListboxOchStatic/ListorListboxOchStatic/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListorListboxOchStatic
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Student s = new Student("Stefan");
+            Student.studentList.Add(s);
+            Student.studentList.Add(new Student("Arne"));
+            Student.studentList.Add(new Student("Berit"));
+            
+
+            Student.addStudent(new Student("Jenna"));
+
+            updateStudentListBox();
+
+            Random myRandom = new Random(12);
+            int a = myRandom.Next();
+
+
+            double aa = Math.PI;
+
+        }
+
+        private void updateStudentListBox()
+        {
+            listBox1.Items.Clear();
+            foreach (Student s in Student.studentList)
+            {
+                listBox1.Items.Add(s);
+                Student.studentList.Add(s);
+            }
+
+            
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Student s = listBox1.SelectedItem as Student;
+            MessageBox.Show("Du valde " + s._firstname);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Student s = listBox1.SelectedItem as Student;
+            Student.studentList.Remove(s);
+            updateStudentListBox();
+            
+        }
+    }
+}
---
+++ ListorListboxOchStatic/ListorListboxOchStatic/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListorListboxOchStatic
+{
+    class Student
+    {
+        public static List<Student> studentList = new List<Student>();
+
+        public string _firstname;
+
+
+        public Student(string firstname)
+        {
+            this._firstname = firstname;
+        }
+
+        public override string ToString()
+        {
+            return "Namn: " + this._firstname;
+        }
+
+        public static void addStudent(Student s)
+        {
+            studentList.Add(s);
+        }
+
+    }
+}
